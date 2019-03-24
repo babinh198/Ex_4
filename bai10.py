d0645974d3095142efb247c3045b1f4f2adabee6
babinh198 <48363973+babinh198@users.noreply.github.com>
2019-03-24 22:34:49 +0700

    Update bai10.py

--- bai10.py
+++ bai10.py
@@ -1,11 +1,9 @@
 
 
 from string import ascii_lowercase
-
 words = ['python', 'patience', 'documents', 'students',
          'homework', 'practice', 'success', 'english',
          'university', 'congratulation']
-
 result = []
 for word in words:
     sum_value = 0
@@ -13,5 +11,4 @@ for word in words:
         value = ascii_lowercase.index(char) + 1
         sum_value = sum_value + value
     result.append([word, sum_value])
-
 print(result)
