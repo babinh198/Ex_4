2087ff0d882af42ff9819dd6f5777031363c7de2
babinh198 <48363973+babinh198@users.noreply.github.com>
2019-03-31 23:19:36 +0700

    Create bai8.py

---
+++ bai8.py
@@ -0,0 +1,11 @@
+samples = ["python", None, "cpython", True, 1, 1 + 1j, False, [100, 200, 300], 99.99, (31, "January")]
+result = []
+for sample in samples:
+    if isinstance(sample, (int, float, complex)) \
+                and sample is not True and sample is not False:
+        result.append(sample)
+print(result)
+
+#####
+
+[1, (1+1j), 99.99]
