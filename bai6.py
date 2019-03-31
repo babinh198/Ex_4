34110d11f9ec960a2adbcd94c809c779f1108733
babinh198 <48363973+babinh198@users.noreply.github.com>
2019-03-31 23:14:00 +0700

    Create bai6.py

---
+++ bai6.py
@@ -0,0 +1,10 @@
+a, b = 1, 1
+fibonacy = [1]
+while b < 1000:
+    a, b = b, a + b
+    fibonacy.append(a)
+print(fibonacy)
+
+#####
+
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
